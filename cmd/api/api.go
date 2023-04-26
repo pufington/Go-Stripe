45ac52b84ceee858e43938a105b9ef1ddb06b164
ganesh <ganesh289katta@gmail.comm>
2023-04-26 21:32:56 +0530

    .

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -55,7 +55,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 8081, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production|maintenance}")
-	flag.StringVar(&cfg.db.dsn, "dsn", "root:ganesh@tcp(localhost:3306)/widgets?parseTime=true&tls=false", "DSN")
+	flag.StringVar(&cfg.db.dsn, "dsn", "root:ganesh@tcp(localhost:3306)/shop?parseTime=true&tls=false", "DSN")
 
 	flag.Parse()
 
